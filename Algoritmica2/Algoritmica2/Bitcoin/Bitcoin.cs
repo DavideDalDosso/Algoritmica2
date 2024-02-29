d1f2833480cdca4bab36dc19de0c0b3ebd49ac92
DavideDalDosso <davidedaldosso08@gmail.com>
2024-02-29 23:57:27 +0100

    Final commit

---
+++ Algoritmica2/Algoritmica2/Bitcoin/Bitcoin.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Bitcoin
+{
+    public void AddValue(float value)
+    {
+        if (value < 0)
+        {
+            Console.WriteLine("Error: not a positive value");
+            return;
+        }
+    }
+}
--- Algoritmica2/Algoritmica2/Bitcoin/ProgramBitcoin.cs
+++ Algoritmica2/Algoritmica2/Bitcoin/ProgramBitcoin.cs
@@ -8,6 +8,22 @@ class ProgramBitcoin
 {
     public static void Main(string[] args)
     {
+        string path = "Input.txt";
 
+        byte[] buffer = new byte[1024];
+        int offset = 0;
+
+        //VVarning! Visual studio creates builds in a subdirectory of the solution folder.
+        //The builds cannot access the resources in the solution files so a work around would
+        //be to copy the file to the build directory on compile on visualstudio post-build event
+        //by running the command COPY or XCOPY
+        //but I ran out of time to figure that out...
+        using (FileStream stream = File.Open(path, FileMode.Open, FileAccess.Read))
+        {
+            while(stream.Read(buffer, offset, buffer.Length) > 0){
+                offset += buffer.Length;
+                Console.WriteLine(buffer);
+            }
+        }
     }
 }
